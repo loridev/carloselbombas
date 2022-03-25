b08dae214466ec4eae6d353d2dddbe2199c2452f
pol223 <poladrover7@gmail.com>
2022-03-25 18:57:20 +0100

    multiplayer2

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+
+    public string VersionName = "0.1";
+    public GameObject UsernameMenu;
+    public GameObject ConnectPanel;
+
+    public InputField UsernameInput;
+    public InputField CreateGameInput;
+    public InputField JoinGameInput;
+
+    public GameObject StartButton;
+
+    void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersionName);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UsernameMenu.SetActive(true);
+    }
+
+    void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+    public void ChangeUserNameInput()
+    {
+        if(UsernameInput.text.Length >= 3)
+        {
+            StartButton.SetActive(true);
+        }
+        else
+        {
+            StartButton.SetActive(false);
+        }
+    }
+
+    public void setUsername()
+    {
+        UsernameMenu.SetActive(false);
+        PhotonNetwork.playerName = UsernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom(CreateGameInput.text, new RoomOptions() { maxPlayers = 5 }, null);
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomoptions = new RoomOptions();
+        roomoptions.maxPlayers = 5;
+        PhotonNetwork.JoinOrCreateRoom(JoinGameInput.text, roomoptions, TypedLobby.Default);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
