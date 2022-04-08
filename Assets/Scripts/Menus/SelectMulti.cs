473491f640c626d94b62bcee048fed1ad29dda27
pol223 <poladrover7@gmail.com>
2022-04-08 16:12:40 +0100

    MenuMulti

---
+++ Assets/Scripts/Menus/SelectMulti.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectMulti : MonoBehaviour
+{
+    public void cargarMundo()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
